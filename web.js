6cb066802a9332d7c547e7f906f02301d76744f3
Jonathan Au <jonathan.au@hulu.com>
2013-07-30 19:33:35 -0700

    Update web.js

--- web.js
+++ web.js
@@ -3,10 +3,12 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  "use strict";
+  response.send('Hello World2!');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
+  "use strict";
   console.log("Listening on " + port);
 });
